2d2cdce88a7a9fe9d76e903b2ecfb1756a60ec8b
muzz-code <uyihope360@gmail.com>
2023-05-02 12:48:52 +0100

    search feature implementation

--- lib/screens/user_blogs_screen.dart
+++ lib/screens/user_blogs_screen.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../provider/blog.dart';
 import '../provider/blog_provider.dart';
 import '../widgets/user_blog_item.dart';
 import './add_edit_blog_screen.dart';
 
-class UserBlogsScreen extends StatelessWidget {
+class UserBlogsScreen extends StatefulWidget {
   static const routeName = '/user-blogs';
+
+  @override
+  _UserBlogsScreenState createState() => _UserBlogsScreenState();
+}
+
+class _UserBlogsScreenState extends State<UserBlogsScreen> {
+  String _searchQuery = '';
+  final _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _onSearchChanged() {
+    setState(() {
+      _searchQuery = _searchController.text;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Blogs>(context);
+    final filteredProducts = productsData.items.where((product) =>
+        product.name.toLowerCase().contains(_searchQuery.toLowerCase()));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Blogs'),
@@ -23,22 +54,41 @@ class UserBlogsScreen extends StatelessWidget {
           ),
         ],
       ),
-      // drawer: AppDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsData.items.length,
-          itemBuilder: (_, i) => Column(
-            children: [
-              UserBlogItem(
-                productsData.items[i].id,
-                productsData.items[i].name,
-                productsData.items[i].imageUrl,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchController,
+                decoration: const InputDecoration(
+                  labelText: 'search',
+                  suffixIcon: Icon(Icons.search),
+                ),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredProducts.length,
+                itemBuilder: (_, i) => Column(
+                  children: [
+                    UserBlogItem(
+                      filteredProducts.toList()[i].id,
+                      filteredProducts.toList()[i].name,
+                      filteredProducts.toList()[i].imageUrl,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
